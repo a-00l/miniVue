1682521d852cbca50bced31782d0d05937f14ea5
雪海-花 <2010433856@qq.com>
2025-09-09 16:37:12 +0800

    feat（reactive）：ref实现

--- src/reactivity/index.js
+++ src/reactivity/index.js
@@ -1,2 +1,3 @@
 export { reactive } from "./reactive.js";
 export { effect, track, trigger } from "./effect.js";
+export { ref } from './ref.js'
---
+++ src/reactivity/ref.js
@@ -0,0 +1,42 @@
+import { hasChanged, isObject } from "../utils/index.js"
+import { reactive, track, trigger } from "./index.js"
+export function ref(value) {
+  // 判断该值是否为ref
+  if (isRef(value)) return value
+
+  return new RefImpl(value)
+}
+
+class RefImpl {
+  constructor(value) {
+    this._value = convert(value)
+    this.__isRef = true // 标记Ref类型
+  }
+
+  get value() {
+    track(this, 'value')
+    return this._value
+  }
+
+  set value(newValue) {
+    // 旧值和新值不同则trigger
+    if (hasChanged(this._value, newValue)) {
+      this._value = convert(newValue)
+      trigger(this, 'value')
+
+      return this._value
+    }
+  }
+}
+
+function isRef(value) {
+  return value && value.__isRef
+}
+
+/**
+ * @description 如果参数使object则用reactive处理，否则返回 
+ * @param {any} value 
+ */
+function convert(value) {
+  return isObject(value) ? reactive(value) : value
+}
