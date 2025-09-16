5d07afb31908030379995fc2fb8416c35ae50316
雪海-花 <2010433856@qq.com>
2025-09-16 20:21:38 +0800

    style(runtime): 为component编写思路注释

--- src/runtime/conponents.js
+++ src/runtime/conponents.js
@@ -16,6 +16,20 @@ function updateProps(vnode, instance) {
   instance.props = reactive(props)
 }
 
+// component实现思路：
+// 1. 定义instance对象，里面包含props、attrs
+// 2. 将vnode中的props和attrs做区分，分别存储到instance中
+// 3. 调用type.setup(props, { attrs })传入必要参数，获取到其return回来的值
+// 4. 定义instance.ctx，将setup中的值和props全都存入到ctx中
+// 5. 挂载component，创建instance.mount = () => {}在这里面实现挂载逻辑
+// 6. 在mount中调用render(instance.ctx)获取到由h函数返回的数据结构
+// 7. 使用normalizeVNode格式化render函数返回的结构（render结构：[]，{}，string | number）
+// 8. 调用patch挂载component
+// 9. 由于component中可能有响应式对象，因此将mount函数替换为update并用effect包裹instance.update = effect(() => {})
+// 10. 定义subTree记录当前节点，使用instance.subTree存储起来
+// 11. 定义prev获取现在的subTree，用来与更新后的subTree做比较，最后调用patch
+// 12. 触发updateComponent时，在mountComponent中我们不知道新节点vnode是什么，所以定义next来存储新节点
+// 13. 使用vnode.component来记录当前component的实例，也好在外面能够使用到组件中的实例，可以用来挂载next
 export function mountComponent(vnode, container, anchor) {
   const { type: Component } = vnode
   // 1. 创建一个实例
@@ -40,12 +54,6 @@ export function mountComponent(vnode, container, anchor) {
     ...instance.setupState
   }
 
-  // 使用subTree记录当前节点
-  // 使用prev来记录还未patch的subTree，也就是上一个节点
-  // 将prev、subTree进行patch
-  // 在updateComponent时，调用update进行组件更新，但是由于这里的节点不知道什么时候更新，所以用next标记
-  // 如果next存在，则进行更新
-  // 最后用instance.subTree来记录当前节点，以及将vnode.el更新为subTree.el
   // 5. 挂载节点
   instance.update = effect(() => {
     // 如果两个组件不同，则更新
