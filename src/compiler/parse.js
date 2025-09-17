dafb1b52c2e7aa05d9f8beaf516e91c89a5efa2b
雪海-花 <2010433856@qq.com>
2025-09-17 17:20:25 +0800

    feat（compiler）：实现parseText（解析文本）功能

--- src/compiler/parse.js
+++ src/compiler/parse.js
@@ -25,6 +25,54 @@ function parseChildren(context) {
   }
 }
 
+/**
+ * @description 解析文本
+ * @param {*} context 
+ * @returns 
+ */
+function parseText(context) {
+  // 遇到 {{ 或者 < 结束文本截取
+  const end = [context.option.delimite[0], '<']
+  let endIndex;
+  // 1. 获取结束位置
+  end.forEach(flag => {
+    const index = context.source.findIndex(flag)
+    if (endIndex > index && index != -1) {
+      endIndex = index
+    }
+  })
+
+  // 2. 获取到文本
+  const text = contentExtraction(context, endIndex)
+  // 3. 截取该文本
+  return {
+    type: NodeTypes.TEXT,
+    content: text
+  }
+}
+
+/**
+ * @description 从开始位置删除length多个字符 
+ * @param {*} context 
+ * @param {*} length 
+ */
+function advance(context, length) {
+  return context.source.slice(length)
+}
+
+/**
+ * @description 从开始位置截取length字符，并且删除截取的字符
+ * @param {*} context 
+ * @param {*} length 
+ * @returns 
+ */
+function contentExtraction(context, length) {
+  const text = context.source.slice(0, length)
+  // 删除字符
+  advance(context, text.length)
+  return text
+}
+
 function isEnd(context) {
   return !context.source.length || context.source.startsWith('</')
 }
